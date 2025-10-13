fd76c9f2ed832f27a9745c54163006766940c331
dannitacode <dannavalenchu076@gmail.com>
2025-10-13 18:40:54 -0300

    Modifique la ruta de los enlaces del header en contact.html. He alineado los titulos de las columnas del mismo. Agregue algunos patrones para probar en validaciones.js

--- contact/validaciones.js
+++ contact/validaciones.js
@@ -1,20 +1,26 @@
 function validaciones() {
 
-    //con esto se sacarian los datos del formulario
-    const formulario = document.getElementById('formulario-contacto')
-    const nombre = "hola"
-    const telefono = formulario.elements['telefono'].value.trim()
-    const email = formulario.elements['email'].value.trim()
-    const mensaje = formulario.elements['mensaje'].value.trim()
+  const formulario = document.getElementById("formulario-contacto");
 
+  const nombre = formulario.elements["nombre-completo"].value.trim();
+  const telefono = formulario.elements["telefono"].value.trim();
+  const email = formulario.elements["correo-electronico"].value.trim();
+  const mensaje = formulario.elements["mensaje"].value.trim();
 
+  const regexNombre = /^[A-Z-a-z-ÁÉÍÓÚáéíóúÑñ\s]+$/
+  const regexTelefono = /^[0-9]{10,14}$/
 
-    //aca iria despues las validaciones de toda la vida parecidas a java
+  //aca iria despues las validaciones de toda la vida parecidas a java
 
-    if(nombre.length < 3){
-        console.log('El nombre debe tener minimo 3 caracteres')
-    }
+  if (nombre.length < 3 || !regexNombre.test(nombre)) {
+    console.log("El nombre debe de tener minimo 3 caracteres y deben ser solo letras.");
+    return;
+  }
+  if (!regexTelefono.test(telefono)) {
+    console.log('El numero tiene que ser de entre 10 y 14 de longitud.')
+    return;
+  }
 
-    //se podria hacer regex para el numero de telefono y demas, permitir solo correos de gmail tmb
-    //hasta ahi llega mi conocimiento
+  //se podria hacer regex para el numero de telefono y demas, permitir solo correos de gmail tmb
+  //hasta ahi llega mi conocimiento
 }
