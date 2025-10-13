7bf7247ddc79d74b111bded4f92424b57d3093a5
MatiasHauria <hauriatute@gmail.com>
2025-10-13 19:15:30 -0300

    "se agregan validaciones'

--- contact/validaciones.js
+++ contact/validaciones.js
@@ -1,7 +1,6 @@
 function validaciones() {
 
   const formulario = document.getElementById("formulario-contacto");
-
   const nombre = formulario.elements["nombre-completo"].value.trim();
   const telefono = formulario.elements["telefono"].value.trim();
   const email = formulario.elements["correo-electronico"].value.trim();
@@ -9,18 +8,35 @@ function validaciones() {
 
   const regexNombre = /^[A-Z-a-z-ÁÉÍÓÚáéíóúÑñ\s]+$/
   const regexTelefono = /^[0-9]{10,14}$/
+  const regexEmail = /\w+@(gmail|hotmail|yahoo|outlook)(\.com|\.edu|\.ar)/
 
+  let valido = true;
   //aca iria despues las validaciones de toda la vida parecidas a java
 
   if (nombre.length < 3 || !regexNombre.test(nombre)) {
-    console.log("El nombre debe de tener minimo 3 caracteres y deben ser solo letras.");
+    alert("El nombre debe de tener minimo 3 caracteres y deben ser solo letras.");
+    valido = false;
     return;
   }
   if (!regexTelefono.test(telefono)) {
-    console.log('El numero tiene que ser de entre 10 y 14 de longitud.')
+    alert('El numero tiene que ser de entre 10 y 14 de longitud.')
+    valido = false;
     return;
   }
+  if(!regexEmail.test(email)){
+    alert('Por favor ingrese un email valido(gmail, hotmail, yahoo, outlook) y asegurese no dejar campos vacios.')
+    valido = false;
+  }
+  if(mensaje.length < 10){
+    alert('Por favor ingrese mas de 10 caracteres para el mensaje')
+    valido = false;
+  }
+  if(valido = true){
+    alert('Se ha enviado el formulario correctamente!')
+    formulario.reset()
+  }else{
+    alert('por favor verifique los campos.')
+  }
 
-  //se podria hacer regex para el numero de telefono y demas, permitir solo correos de gmail tmb
-  //hasta ahi llega mi conocimiento
+
 }
