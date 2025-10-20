f5fc06db81457ad7282a5ba838ac76cc67ffc248
dannitacode <dannavalenchu076@gmail.com>
2025-10-20 03:25:29 -0300

    Agregando validaciones correctas para los campos del formulario en la págica 'Trabaja con nosotros'. La idea está, la base está, falta que sea responsivo. Matute, all in your hands ;)

--- contact/validaciones.js
+++ contact/validaciones.js
@@ -1,37 +1,74 @@
-function validaciones() {
-
-  const formulario = document.getElementById("formulario-contacto");
-  const nombre = formulario.elements["nombre-completo"].value.trim();
-  const telefono = formulario.elements["telefono"].value.trim();
-  const email = formulario.elements["correo-electronico"].value.trim();
-  const mensaje = formulario.elements["mensaje"].value.trim();
+const formulario = document.forms['formulario']
+  const nombreInput = formulario['nombre-completo']
+  const telefonoInput = formulario['telefono']
+  const emailInput = formulario['correo-electronico']
+  const mensajeInput = formulario['mensaje']
 
   const regexNombre = /^[A-Z-a-z-ÁÉÍÓÚáéíóúÑñ\s]+$/
   const regexTelefono = /^[0-9]{10,14}$/
-  const regexEmail = /\w+@(gmail|hotmail|yahoo|outlook)(\.com|\.edu|\.ar)/
+  const regexEmail = /^(\w)+@(gmail|hotmail|yahoo|outlook)(\.com|\.ar)$/
 
-  /* Quité el booleano y en lugar retorné false o true según el caso. El booleano revisa TODOS los campos antes de mostrar algo, y tipo saltan todas las alertas al final. Entonces con el return false, detiene  el flujo a la primera y podes corregir una por una, y no saltan todas las alertas de golpe al final*/
-  /* let valido = true; */
+function validaciones() {
+  const spanNombre = document.getElementById('error-nombre')
+  const spanTelefono = document.getElementById('error-telefono')
+  const spanCorreo = document.getElementById('error-correo')
+  const spanMensaje = document.getElementById('error-mensaje')
+  const nombreValue = nombreInput.value
+  const telefonoValue = telefonoInput.value
+  const emailValue = emailInput.value
+  const mensajeValue = mensajeInput.value
+  let bandera = true
 
 
-  if (nombre.length < 3 || !regexNombre.test(nombre)) {
-    alert("El nombre debe de tener minimo 3 caracteres y deben ser solo letras.");
-    return false;
-  }
-  if (!regexTelefono.test(telefono)) {
-    alert('El numero tiene que ser de entre 10 y 14 de longitud.')
-    return false;
-  }
-  if(!regexEmail.test(email)){
-    alert('Por favor ingrese un email valido(gmail, hotmail, yahoo, outlook) y asegurese no dejar campos vacios.')
-    return false;
-  }
-  if(mensaje.length < 10){
-    alert('Por favor ingrese mas de 10 caracteres para el mensaje')
-    return false;
+  if (nombreValue.length < 3 || !regexNombre.test(nombreValue) || nombreValue === '') {
+    nombreInput.style.borderColor = "red"
+    spanNombre.textContent = 'El nombre debe de tener minimo 3 caracteres y deben ser solo letras.'
+    spanNombre.style.textAlign = "left"
+    spanNombre.style.top = '21%'
+    spanNombre.style.display = 'block'
+    bandera = false
+  } else {
+    nombreInput.style.borderColor = "black"
+    spanNombre.textContent = ""
   }
 
-    alert('Se ha enviado el formulario correctamente!')
+  if (!regexTelefono.test(telefonoValue) || (telefonoValue.length < 10 || telefonoValue.length > 14) || telefonoValue === '') {
+    telefonoInput.style.borderColor = "red"
+    spanTelefono.textContent = 'El numero tiene que ser de entre 10 y 14 de longitud, sin guiones.'
+    spanTelefono.style.textAlign = "left"
+    spanTelefono.style.top = '38%'
+    spanTelefono.style.display = 'block'
+    bandera = false
+  } else {
+    telefonoInput.style.borderColor = "black"
+    spanTelefono.textContent = ""
+  }
+
+  if (!regexEmail.test(emailValue) || emailValue === '') {
+    emailInput.style.borderColor = "red"
+    spanCorreo.textContent = 'El correo debe de ser valido (gmail, yahoo, outlook, hotmail)'
+    spanCorreo.style.textAlign = "left"
+    spanCorreo.style.top = '55%'
+    spanCorreo.style.display = 'block'
+    bandera = false
+  } else {
+    emailInput.style.borderColor = "black"
+    spanCorreo.textContent = ""
+  }
+
+  if (mensajeValue === '' || mensajeValue.length < 100) {
+    mensajeInput.style.borderColor = "red"
+    spanMensaje.textContent = 'El mensaje debe de ser mayor a 100 caracteres.'
+    spanMensaje.style.textAlign = "left"
+    spanMensaje.style.bottom = '73px'
+    spanMensaje.style.display = 'block'
+    bandera = false
+  } else {
+    mensajeInput.style.borderColor = "black"
+    spanMensaje.textContent = ""
+  }
+
+  if(bandera === true) alert('Formulario enviado exitosamente!')
     formulario.reset()
-    return true;
+  return false;
 }
