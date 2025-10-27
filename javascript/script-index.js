b4beb908daeca899fd079b7b9b539bec7a574e82
dannitacode <dannavalenchu076@gmail.com>
2025-10-27 03:24:27 -0300

    He agregado un pequeño codigo en el script del índice que permite, con el flujo del carrousel, determinar que bebida esta mostrando, con el uso de un arreglo. Tambien agregue unos botones secundarios ya que a la hora de achicar la pantalla, los del costado quedan muy pegados a los laterales.

--- javascript/script-index.js
+++ javascript/script-index.js
@@ -1,23 +1,35 @@
+const tipos = [
+    'Cappuccino',
+    'Flat White',
+    'Caramel Macchiato',
+    'Americano',
+    'Affogato'
+]
+
 const imagenes = [
-        './img/cafe-cappuccino.jpg',
-        './img/cafe-flat-white.jpg',
-        './img/cafe-caramel.jpg',
-        './img/cafe-americano.jpg',
-        './img/cafe-affogato.jpg'
-    ]
-    let contador = 0
-    let imagen = document.getElementById('imagen')
+    './img/cafe-cappuccino.jpg',
+    './img/cafe-flat-white.jpg',
+    './img/cafe-caramel.jpg',
+    './img/cafe-americano.jpg',
+    './img/cafe-affogato.jpg'
+]
+let contador = 0
+const imagen = document.getElementById('imagen')
+const tipo = document.getElementById('tipo-cafe')
 function carrousel() {
     contador++
     if (contador === 5) {
         contador = 0
     }
     imagen.src = imagenes[contador]
+    tipo.textContent = tipos[contador]
 }
+
 function carrouselBack() {
     contador--
     if (contador === -1) {
         contador = 4
     }
     imagen.src = imagenes[contador]
+    tipo.textContent = tipos[contador]
 }
